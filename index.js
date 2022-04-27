a416637562a6f608f8e6a0b5b44e1f762b98490f
sgr98 <60283755+sgr98@users.noreply.github.com>
2022-04-27 21:00:35 +0530

    Rand_Commit 1

--- index.js
+++
@@ -1,4 +0,0 @@
-// dummy funciton for checking and publishing
-module.exports.add = function(a,b){
-    return a+b;
-}
---
+++ index.mjs
@@ -0,0 +1,7 @@
+// dummy funciton for checking and publishing
+// module.exports.add = function(a,b){
+//     return a+b;
+// }
+export const add = (a, b) => {
+    return a + b;
+}
