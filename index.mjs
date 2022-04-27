cc776808d14ed187fbc5c5a67b0dd23633952e12
sgr98 <60283755+sgr98@users.noreply.github.com>
2022-04-27 23:24:11 +0530

    Transfered coffee_break files to package

--- index.mjs
+++ index.mjs
@@ -1,7 +1,14 @@
-// dummy funciton for checking and publishing
-// module.exports.add = function(a,b){
-//     return a+b;
-// }
-export const add = (a, b) => {
-    return a + b;
-}
+import createServerFile from "./create_server/index.mjs";
+import createRouterFile from "./create_router/index.mjs";
+import createObjectFile from "./create_object/index.mjs";
+
+const generate_test_files = test_settings => {
+	createRouterFile(test_settings.router_settings);
+	createServerFile(
+		test_settings.server_settings,
+		test_settings.router_settings.router_file_name
+	);
+	createObjectFile(test_settings.test_settings, test_settings.schema, test_settings.n_intentional_right_cases);
+};
+
+export default generate_test_files;
